9bf4a545158b9d1cd738997aef436f38faf0c5c1
Praful-Daksh <prafuldaksh@proton.me>
2025-04-18 13:04:28 +0530

    ,,

--- backend/routes/AuthRouter.js
+++ backend/routes/AuthRouter.js
@@ -3,7 +3,7 @@ const Authrouter = express.Router();
 const { registerValidation, LoginValidation } = require('../Middleware/AuthValidation.js')
 const { signUp, logIn } = require('../Controllers/AuthController.js')
 
-router.post('/login', LoginValidation, logIn);
-router.post('/register', registerValidation, signUp)
+Authrouter.post('/login', LoginValidation, logIn);
+Authrouter.post('/register', registerValidation, signUp)
 
 module.exports = Authrouter
